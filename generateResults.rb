335f0d6ac26af82e842adc8e92505672f3369863
ma1twn@BATH.AC.UK <ma1twn@BATH.AC.UK@ea719f28-4a97-0410-84ec-d58565bab16c>
2012-10-26 10:38:59 +0000

    moved core code into an object

--- generateResults.rb
+++ generateResults.rb
@@ -1,91 +1,12 @@
 #!/bin/ruby
 
 require 'rubygems'
-require 'rss/maker'
-require 'yaml'
+require 'test_runner.rb'
+
 
 # config
 script_location = File.expand_path(File.dirname(__FILE__))
 
-test_location = script_location + "/tests/"
-output_file = script_location + "/output/service_status.rss" # local file to write
-
-# load all test types
-Dir.new(script_location + "/types/").entries.each { |file|
-    if file =~ /.+\.rb$/ then
-        require "#{script_location}/types/#{file}"
-    end
-}
-
-# check for output directory
-if (!(File.exists?(File.dirname(output_file)) && File.directory?(File.dirname(output_file)))) then
-    abort("you need an output directory")
-end
-
-results = Array.new
-
-# for each file do 
-Dir.new(test_location).entries.each { |file|
-    
-    # only read files ending in .txt
-    if file =~ /.+\.yml$/ then 
-    
-        file = test_location + file
-        
-        # read the yaml into a string - there must be a better way of doing this
-        data = ''
-        f = File.open(file, "r") 
-        f.each_line do |line|
-            data += line
-        end
-        
-        test = YAML::load(data)
-        
-        if (test["type"] == "website") then 
-            w = WebsiteTest.new(test["name"], test["url"])
-            results << w.run_test
-        elsif (test["type"] == "mysql") then
-            m = MysqlTest.new
-            results << m.run_test
-        elsif (test["type"] == "oracle") then
-            o = OracleTest.new
-            results << o.run_test
-        else
-            results << ["other", "http://www.example.com", false.to_s]
-        end
-    end
-    # end the for each
-}
-
-# create the output RSS feed 
-version = "2.0" # ["0.9", "1.0", "2.0"]
-
-content = RSS::Maker.make(version) do |m|
-    m.channel.title = "Web Services Services. On the Web."
-    m.channel.link = "http://www.bath.ac.uk/"
-    m.channel.description = "Status of the services run by Web Services"
-    m.items.do_sort = true # sort items by date
-    
-    # for each result, add an entry in the output feed
-    results.each { |result|
-        i = m.items.new_item
-        i.title = result[0]
-        i.link = result[1]
-        i.description = result[2]
-        i.date = Time.now    
-    }
-    
-end
-
-File.open(output_file,"w") do |f|
-    f.write(content)
-end
-
-def get_file_as_string(filename)
-    data = ''
-    f = File.open(filename, "r") 
-    f.each_line do |line|
-        data += line
-    end
-    return data
-end
+tests = TestRunner.new(script_location)
+tests.run_tests
+tests.create_output
---
+++ test_runner.rb
@@ -0,0 +1,105 @@
+require 'rss/maker'
+require 'yaml'
+
+class TestRunner
+
+    attr_reader :script_location, :test_location, :output_file
+
+    def initialize(script_location)
+        @script_location = script_location
+        @test_location = @script_location + "/tests/" # location of test files
+        @output_file = @script_location + "/output/service_status.rss" # local file to write
+
+        # load all test types
+        Dir.new(script_location + "/types/").entries.each { |file|
+            if file =~ /.+\.rb$/ then
+                require "#{script_location}/types/#{file}"
+            end
+        }
+
+        # check for output directory
+        if (!(File.exists?(File.dirname(output_file)) && File.directory?(File.dirname(output_file)))) then
+            raise("you need an output directory")
+        end
+
+        # somewhere to put the output
+        @results = Array.new
+        
+    end
+
+    def run_tests
+    
+        # for each file do 
+        Dir.new(@test_location).entries.each { |file|
+            
+            # only read files ending in .txt
+            if file =~ /.+\.yml$/ then 
+
+                # eugh...
+                test = read_yaml_file(@test_location + file)
+                
+                if (test["type"] == "website") then 
+                    w = WebsiteTest.new(test["name"], test["url"])
+                    @results << w.run_test
+                elsif (test["type"] == "mysql") then
+                    m = MysqlTest.new
+                    @results << m.run_test
+                elsif (test["type"] == "oracle") then
+                    o = OracleTest.new
+                    @results << o.run_test
+                else
+                    @results << ["other", "http://www.example.com", false.to_s]
+                end
+            end
+            # end the for each
+        }
+
+    end
+
+    def create_output
+    
+        # create the output RSS feed 
+        version = "2.0" # ["0.9", "1.0", "2.0"]
+
+        content = RSS::Maker.make(version) do |m|
+            m.channel.title = "Web Services Services. On the Web."
+            m.channel.link = "http://www.bath.ac.uk/"
+            m.channel.description = "Status of the services run by Web Services"
+            m.items.do_sort = true # sort items by date
+            
+            # for each result, add an entry in the output feed
+            @results.each { |result|
+                i = m.items.new_item
+                i.title = result[0]
+                i.link = result[1]
+                i.description = result[2]
+                i.date = Time.now    
+            }
+            
+        end
+
+        File.open(output_file,"w") do |f|
+            f.write(content)
+        end
+        
+    end
+
+    private
+    
+        #  there must be a better way of doing this...
+        def read_yaml_file(filename)
+            # read the yaml into a string
+            data = ''
+            f = File.open(filename, "r") 
+            f.each_line do |line|
+                data += line
+            end
+            # parse the yaml-string
+            test = YAML::load(data)
+            return test
+        end
+
+end
+
+
+
