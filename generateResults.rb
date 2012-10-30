2931e791a1beed0e3112ce9cd59d0e21a55eb494
ma1twn@BATH.AC.UK <ma1twn@BATH.AC.UK@ea719f28-4a97-0410-84ec-d58565bab16c>
2012-10-30 11:00:59 +0000

    added the POWER OF SINATRA

--- generateResults.rb
+++ generateResults.rb
@@ -2,11 +2,19 @@
 
 require 'rubygems'
 require 'test_runner.rb'
+require 'sinatra'
 
 
 # config
 script_location = File.expand_path(File.dirname(__FILE__))
-
 tests = TestRunner.new(script_location)
-tests.run_tests
-tests.create_output
+
+get '/run' do
+    tests.run_tests
+    tests.create_output
+    puts "done"
+end
+
+get '/output' do
+    send_file File.join(settings.public_folder, 'index.html')
+end
