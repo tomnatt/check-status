75d1ec038a551d62f97639cc75f0b01fc3f87534
ma1twn@BATH.AC.UK <ma1twn@BATH.AC.UK@ea719f28-4a97-0410-84ec-d58565bab16c>
2012-11-07 13:49:12 +0000

    rack-ified everything

--- myapp.rb
+++ myapp.rb
@@ -7,46 +7,50 @@ require 'sinatra'
 require 'rss/parser'
 require 'rufus/scheduler'
 
-# config
-script_location = File.expand_path(File.dirname(__FILE__))
+class MyApp < Sinatra::Base
 
-# set up web routes
-tests = TestRunner.new(script_location)
+    # config
+    script_location = File.expand_path(File.dirname(__FILE__))
 
-get '/run' do
-    tests.run_tests
-    tests.create_output
-    "done"
-end
+    # set up web routes
+    tests = TestRunner.new(script_location)
 
-get '/output' do
-    send_file tests.output_file
-end
-
-get '/' do
-
-    # Read the feed into rss_content
-    rss_content = ""
-    open(tests.output_file, "r") do |f|
-       rss_content = f.read
+    get '/run' do
+        tests.run_tests
+        tests.create_output
+        "done"
     end
 
-    # Parse the feed, dumping its contents to rss
-    rss = RSS::Parser.parse(rss_content, false)
+    get '/output' do
+        send_file tests.output_file
+    end
 
-    title = "Is it up?"
-    date = "Last updated: " + Time.parse(rss.items[0].date.to_s).strftime("%H:%M:%S %d/%m/%Y")
-    haml :index, :format => :html5, :locals => {:title => title, :date => date, :rss => rss}
-end
+    get '/' do
 
-get '/test' do
-    puts "test2"
-end
+        # Read the feed into rss_content
+        rss_content = ""
+        open(tests.output_file, "r") do |f|
+           rss_content = f.read
+        end
 
-# set up scheduling - refactor to a different file?
-scheduler = Rufus::Scheduler.start_new
+        # Parse the feed, dumping its contents to rss
+        rss = RSS::Parser.parse(rss_content, false)
 
-scheduler.every '10m' do
-    tests.run_tests
-    tests.create_output
+        title = "Is it up?"
+        date = "Last updated: " + Time.parse(rss.items[0].date.to_s).strftime("%H:%M:%S %d/%m/%Y")
+        haml :index, :format => :html5, :locals => {:title => title, :date => date, :rss => rss}
+    end
+
+    get '/test' do
+        puts "test3"
+    end
+
+    # set up scheduling - refactor to a different file?
+    scheduler = Rufus::Scheduler.start_new
+
+    scheduler.every '10m' do
+        tests.run_tests
+        tests.create_output
+    end
+
 end
