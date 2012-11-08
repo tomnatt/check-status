de9f4c4958e8fea55cee1269425909b77ab8ed50
tomnatt <github@tomnatt.com>
2012-11-08 16:10:02 +0000

    support ssl in web addresses and give a bit more feedback on status screen

--- types/website_test.rb
+++ types/website_test.rb
@@ -1,5 +1,6 @@
 require File.expand_path(File.dirname(__FILE__)) + "/base_test.rb"
 require 'net/http'
+require 'net/https'
 
 class WebsiteTest < BaseTest
 
@@ -15,14 +16,21 @@ class WebsiteTest < BaseTest
         url = URI.parse(@addr)
         # can add proxy here if we need it later
         http = Net::HTTP.new(url.host, url.port)
+
+        # switch to ssl if required
+        if (url.scheme == "https") then
+            http.use_ssl = true
+        end
         res = http.request(Net::HTTP::Get.new(url.request_uri))
         
-        @result = false
-        if (res.code.to_i < 400) then 
-            @result = true
+        @result = "website:down"
+        if (res.code.to_i >= 300 && res.code.to_i < 400) then
+            @result = "website:redirecting"
+        elsif (res.code.to_i < 300) then 
+            @result = "website:up"
         end
 
-        return [@name, @addr, @result.to_s]
+        return [@name, @addr, @result]
     end
 
 end
