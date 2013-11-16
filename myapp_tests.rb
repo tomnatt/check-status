8e945d181bdd34c3c53464665398d79f2dea4228
Phil Wilson <github@philwilson.org>
2013-11-16 14:30:04 +0000

    trying different test config

--- myapp_tests.rb
+++ myapp_tests.rb
@@ -8,12 +8,13 @@ class MyAppTest < Test::Unit::TestCase
   include Rack::Test::Methods
 
   def app
-    Sinatra::Application
+    @app ||= MyApp.new
   end
 
   def test_it_returns_ok
     get '/test'
-    puts last_response
+    puts last_response.body
+    assert last_response.ok?
     assert_equal 'test3', last_response.body
   end
 
